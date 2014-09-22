78e7b9ca3f408593b60ebf9d1f5cc195aba67237
MJ Madison-Villar <mj.madisonvillar@gmail.com>
2014-09-22 14:59:57 -0700

    Extracts specified subset of entries
    inputs: 1.  csv file of FASTA converted sequences ("f")
    2.  txt file of references/identifiers contained in desired sequence to
    extract ("r")
    outputs: 1.  csv file of extracted sequences

---
+++ extract_sequences_csv.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 21 19:22:06 2014
+
+@author: MJMV
+"""
+
+
+## file containing sequences in csv format
+## change path to desired file
+f = open('C:/Users/') 
+
+## file containing identifier for sequences to extract (names, motifs, etc.) 
+## file contains each identifier on a separate line
+## change path to desired file
+r = open('C:/Users/') 
+
+## output file to contain extracted sequences
+## change name to desired name/location
+o= open('C:/Users/', 'w') 
+
+
+taxa = []
+entries = []
+
+for entry in f:
+    if entry not in entries:
+        entries.append(entry)
+
+for taxon in r:
+    taxon2=taxon.rstrip("\n")
+    if taxon2 not in taxa:
+        taxa.append(taxon2)
+      
+for taxon in taxa:
+    for entry in entries:
+        if taxon in entry:
+            o.write(entry)
+
+o.close()
+      
+   
